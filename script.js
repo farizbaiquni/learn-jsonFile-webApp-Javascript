385db697838214f94932955a9a7da694e4f4585e
unknown <farizbaiquni@gmail.com>
2021-05-11 00:16:51 +0700

    Add not found information and refactoring code

--- script.js
+++ script.js
@@ -13,14 +13,10 @@ function getContent(response){
 }
 
 
-//Run in first load to display all menu
-getContent(response => {
-    console.log(response.menu)
-    let html = ''
-    response.menu.map(data => {
-             html += `
-             <div class="col-sm-3 mb-3">
-                 <div class="card h-100">
+// ================== TEMPLATING FOR CARD MOVIE================== 
+function templateCardMovie(data){
+    return ` <div class="col-sm-3 mb-3">
+                <div class="card h-100">
                     <img src="/img/pizza/${data.gambar}" class="card-img-top" alt="...">
                     <div class="card-body" style ="position: relative">
                         <h5 class="card-title">${data.nama}</h5>
@@ -29,16 +25,27 @@ getContent(response => {
                         <h3 style ="position: absolute; bottom: 0;">Rp.${data.harga}</h3>
                     </div>
                 </div>
-             </div>
-             `
+            </div>
+    `
+}
+
+
+// ================== DISPLAY ALL MENU AT THE FIRST RENDERING ================== 
+getContent(response => {
+    let html = ''
+    response.menu.map(data => {
+             html += templateCardMovie(data)
     })
     divContent.innerHTML = html
 })
 
 
 
-//Code to reacticate active classs base on user click and
-//adjust content base on menu that user clicked
+
+// ================== FILTER CONTENT BASED ON CATEGORY MENU================== 
+
+//Code to reacticate active classs in menu element when user click category menu and
+//filter content based on category menu that user clicked
 document.querySelectorAll('.nav-link').forEach(list => {
     list.addEventListener("click", function(){
         document.querySelector(".active").classList.remove("active")
@@ -46,45 +53,26 @@ document.querySelectorAll('.nav-link').forEach(list => {
 
         //If menu is "All Menu" then run this code
         if(this.dataset.menu == "all"){
-            getContent(response => {
+            getContent(response => {   
                 let html = ''
                 response.menu.map(data => {
-                         html += `
-                         <div class="col-sm-3 mb-3">
-                            <div class="card h-100">
-                                <img src="/img/pizza/${data.gambar}" class="card-img-top" alt="...">
-                                <div class="card-body" style ="position: relative">
-                                    <h5 class="card-title">${data.nama}</h5>
-                                    <p class="card-text">${data.deskripsi}</p>
-                                    <br>
-                                    <h3 style ="position: absolute; bottom: 0;">Rp.${data.harga}</h3>
-                                </div>
-                            </div>
-                        </div>
-                         `
+                         html += templateCardMovie(data)
                 })
                 divContent.innerHTML = html
             })
         } else {
             getContent(response => {
                 let html = ''
-                //First filter data based on menu the mapping
-                response.menu.filter(data => data.kategori == this.dataset.menu).map(data => {
-                    html += `
-                    <div class="col-sm-3 mb-3">
-                        <div class="card h-100">
-                            <img src="/img/pizza/${data.gambar}" class="card-img-top" alt="...">
-                            <div class="card-body" style ="position: relative">
-                                <h5 class="card-title">${data.nama}</h5>
-                                <p class="card-text">${data.deskripsi}</p>
-                                <br>
-                                <h3 style ="position: absolute; bottom: 0;">Rp.${data.harga}</h3>
-                            </div>
-                        </div>
-                    </div>
-                    `
+                //filter data based on category menu
+                let result = response.menu.filter(data => data.kategori == this.dataset.menu).map(data => {
+                    html += templateCardMovie(data)
                  })
+
+                if(result.length == 0){
+                    html += `<h3 class="text-center mt-3"> Data not Found </h3>`
+                } 
                 divContent.innerHTML = html
+                
             })
         }
 
@@ -104,60 +92,42 @@ document.querySelectorAll('.nav-link').forEach(list => {
 // }
 
 
-let buttonSearch = document.querySelector('#button-search')
 
+// ================== EVENT FOR SEARCH INPUT ==================
+let buttonSearch = document.querySelector('#button-search')
+//User click search button
 buttonSearch.addEventListener("click", function(e){
     let html = '';
     e.preventDefault();
     let inputKeyword = document.getElementById("input-keyword").value;
     document.querySelector(".nav-link").classList.remove("active")
     getContent(response => {
-        response.menu.filter(data => data.nama.toLowerCase().indexOf(inputKeyword.toLowerCase()) > -1 ).map(data => {
+        let result = response.menu.filter(data => data.nama.toLowerCase().indexOf(inputKeyword.toLowerCase()) > -1 ).map(data => {
             console.log(data)
-            html += `
-            <div class="col-sm-3 mb-3">
-                <div class="card h-100">
-                    <img src="/img/pizza/${data.gambar}" class="card-img-top" alt="...">
-                    <div class="card-body" style ="position: relative">
-                        <h5 class="card-title">${data.nama}</h5>
-                        <p class="card-text">${data.deskripsi}</p>
-                        <br>
-                        <h3 style ="position: absolute; bottom: 0;">Rp.${data.harga}</h3>
-                    </div>
-                </div>
-            </div>
-                 `
+            html += templateCardMovie(data)
             })
 
-            divContent.innerHTML = html;
+        if(result.length == 0){
+            html += `<h3 class="text-center mt-3"> Data not Found </h3>`
+        } 
+        divContent.innerHTML = html
     })
 })
-
+//User type in search button
 document.getElementById("input-keyword").addEventListener("keyup", function(e){
-    console.log("object")
     let html = '';
     e.preventDefault();
     let inputKeyword = document.getElementById("input-keyword").value;
     document.querySelector(".nav-link").classList.remove("active")
-    console.log(inputKeyword.search("Beef"))
     getContent(response => {
-        response.menu.filter(data => data.nama.toLowerCase().indexOf(inputKeyword.toLowerCase()) > -1).map(data => {
-            console.log(data)
-            html += `
-            <div class="col-sm-3 mb-3">
-                <div class="card h-100">
-                    <img src="/img/pizza/${data.gambar}" class="card-img-top" alt="...">
-                    <div class="card-body" style ="position: relative">
-                        <h5 class="card-title">${data.nama}</h5>
-                        <p class="card-text">${data.deskripsi}</p>
-                        <br>
-                        <h3 style ="position: absolute; bottom: 0;">Rp.${data.harga}</h3>
-                    </div>
-                </div>
-            </div>
-                 `
-            })
-
-            divContent.innerHTML = html;
+        let result = response.menu.filter(data => data.nama.toLowerCase().indexOf(inputKeyword.toLowerCase()) > -1).map(data => {
+                        html += templateCardMovie(data)
+                    })
+        if(result.length == 0){
+            html += `<h3 class="text-center mt-3"> Data not Found </h3>`
+        } 
+        divContent.innerHTML = html
+        
+        
     })
 })
